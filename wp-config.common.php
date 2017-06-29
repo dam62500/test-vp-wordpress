508b9b44b919a3dda6c661be91eb84c2a707dd93
Damien Flandrin <dam.62500@gmail.com>
2017-06-29 16:58:54 +0200

    ok

--- wp-config.common.php
+++ wp-config.common.php
@@ -1,3 +1,18 @@
 <?php
 // Configuration common to all VersionPress environments, included from `wp-config.php`.
 // Learn more at https://docs.versionpress.net/en/getting-started/configuration
+// ** Réglages MySQL - Votre hébergeur doit vous fournir ces informations. ** //
+/** Nom de la base de données de WordPress. */
+define('DB_NAME', 'wordpress');
+
+/** Utilisateur de la base de données MySQL. */
+define('DB_USER', 'root');
+
+/** Mot de passe de la base de données MySQL. */
+define('DB_PASSWORD', 'carredasicetea');
+
+/** Adresse de l’hébergement MySQL. */
+define('DB_HOST', 'localhost');
+
+/** Jeu de caractères à utiliser par la base de données lors de la création des tables. */
+define('DB_CHARSET', 'utf8mb4');
